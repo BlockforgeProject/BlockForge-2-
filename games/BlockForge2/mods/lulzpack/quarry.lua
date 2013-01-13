9f176872ff9c89619036712e24e06f2586677570
BlockforgeProject <lorenzo.ct@alice.it>
2013-01-13 11:18:58 +0100

    Update games/BlockForge2/mods/lulzpack/quarry.lua

--- games/BlockForge2/mods/lulzpack/quarry.lua
+++ games/BlockForge2/mods/lulzpack/quarry.lua
@@ -89,7 +89,7 @@ if meta:get_inventory():contains_item("drill", "lulzpack:quarrydrill") then
                     end
                 end                    
             end
-            for i=1, 9 do
+            --[[for i=1, 9 do
                 if nodenames[i].name == 'default:chest_locked' or nodenames[i].name == 'default:chest' then
                     local chest_meta = minetest.env:get_meta(quarryposs[i])
                     local chest_inv = chest_meta:get_inventory()
@@ -108,7 +108,7 @@ if meta:get_inventory():contains_item("drill", "lulzpack:quarrydrill") then
                             inv:add_item("main",item:get_name()) end end
                        end  
                     end    
-                end  
+                end]]  
 			    quarry_mine(quarryposs[i])           
             end
             quarry_pos["q"..pos.x.."-"..pos.z]=quarry_pos["q"..pos.x.."-"..pos.z]+1
