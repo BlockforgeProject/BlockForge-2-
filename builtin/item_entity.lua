78f5d9e31c9a8933ce3b40337ab2a0a59d3a5c14
BlockforgeProject <lorenzo.ct@alice.it>
2013-01-13 16:46:02 +0100

    Update builtin/item_entity.lua

--- builtin/item_entity.lua
+++ builtin/item_entity.lua
@@ -129,7 +129,7 @@ minetest.register_entity("__builtin:item", {
                                         if item~=nil and hitter~=nil and item:get_luaentity().itemstring~=nil then
                                             hitter:get_inventory():add_item("main", ItemStack(item:get_luaentity().itemstring)) 
                                             item:remove()
-                                            minetest.sound_play({name="pickup",gain=0.2},sound_pickup)
+                                            minetest.sound_play({name="pickup",gain=0.2,max_hear_distance=2*12},sound_pickup)
                                         end end, {item,hitter})                    
                 end
             end
